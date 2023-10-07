e84f5b9b13b47eac0b2fece3acb208b357163798
eunho <jeh4916@gmail.com>
2023-10-07 13:15:10 +0900

    Step #7: Server delivery

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+// open the database file
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_offset TEXT UNIQUE,
+    content TEXT
+  );
+`);
 
 const app = express();
 const server = createServer(app);
@@ -12,11 +29,34 @@ app.get("/", (req, res) => {
   res.sendFile(new URL("./index.html", import.meta.url).pathname);
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    // this will emit the event to all connected sockets
-    io.emit("chat message", msg);
+io.on("connection", async (socket) => {
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      // store the message in the database
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
+    // include the offset with the message
+    io.emit("chat message", msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (e) {
+      // something went wrong
+    }
+  }
 });
 
 server.listen(3000, () => {
