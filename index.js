2a74e82da2afa1dd2374f4ccc49e4569001d42b1
eunho <jeh4916@gmail.com>
2023-10-07 15:44:54 +0900

    Ending notes

--- index.js
+++ index.js
@@ -9,22 +9,19 @@ import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
 if (cluster.isPrimary) {
   const numCPUs = availableParallelism();
-  // create one worker per available core
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork({
       PORT: 3000 + i,
     });
   }
-  // set up the adapter on the primary thread
+
   setupPrimary();
 } else {
-  // open the database file
   const db = await open({
     filename: "chat.db",
     driver: sqlite3.Database,
   });
 
-  // create our 'messages' table (you can ignore the 'client_offset' column for now)
   await db.exec(`
     CREATE TABLE IF NOT EXISTS messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -37,9 +34,9 @@ if (cluster.isPrimary) {
   const server = createServer(app);
   const io = new Server(server, {
     connectionStateRecovery: {},
-    // set up the adapter on each worker thread
     adapter: createAdapter(),
   });
+
   app.get("/", (req, res) => {
     res.sendFile(new URL("./index.html", import.meta.url).pathname);
   });
@@ -48,7 +45,6 @@ if (cluster.isPrimary) {
     socket.on("chat message", async (msg, clientOffset, callback) => {
       let result;
       try {
-        // store the message in the database
         result = await db.run(
           "INSERT INTO messages (content, client_offset) VALUES (?, ?)",
           msg,
@@ -56,7 +52,6 @@ if (cluster.isPrimary) {
         );
       } catch (e) {
         if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
-          // the message was already inserted, so we notify the client
           callback();
         } else {
           // nothing to do, just let the client retry
@@ -64,12 +59,10 @@ if (cluster.isPrimary) {
         return;
       }
       io.emit("chat message", msg, result.lastID);
-      // acknowledge the event
       callback();
     });
 
     if (!socket.recovered) {
-      // if the connection state recovery was not successful
       try {
         await db.each(
           "SELECT id, content FROM messages WHERE id > ?",
